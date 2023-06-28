5c54f11c5f2cf14671cdf88e84fc83606f14a1bf
AgostinaCipolla <133062972+AgostinaCipolla@users.noreply.github.com>
2023-06-28 14:00:29 -0300

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -23,3 +23,25 @@ function oscuro() { document.body.style.backgroundColor="#395667";
                     document.getElementById("seguircoc").style.color="#CCCCCC";
                     document.getElementById("footer").style.backgroundColor="#15202B";
  }
+
+ function claro() { document.body.style.backgroundColor="#ffffdd";
+                     document.getElementById("header").style.backgroundColor="#e4d6f8";
+                     document.getElementById("nav").style.backgroundColor="#f4c4e9";
+                     document.getElementById("navletra1").style.color="black";
+                     document.getElementById("navletra2").style.color="black";
+                     document.getElementById("navletra3").style.color="black";
+                     document.getElementById("navletra4").style.color="black";
+                     document.getElementById("texto").style.color="black";
+                     document.getElementById("section").style.backgroundColor="#fad3b2";
+                     document.getElementById("section2").style.backgroundColor="#fad3b2";
+                     document.getElementById("titulo").style.color="#021b2b";
+                     document.getElementById("textohis").style.color="black";
+                     document.getElementById("section3").style.backgroundColor="#fad3b2";
+                     document.getElementById("ingredlis").style.color="black";
+                     document.getElementById("pasoslis").style.color="black";
+                     document.getElementById("section4").style.backgroundColor="#fad3b2";
+                     document.getElementById("videotitu").style.color="#ce7095";
+                     document.getElementById("section5").style.backgroundColor="#fad3b2";
+                     document.getElementById("seguircoc").style.color="black";
+                     document.getElementById("footer").style.backgroundColor="#e4c7dd";
+  }
