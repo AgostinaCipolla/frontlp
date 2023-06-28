a240dc0c5950de3642362979146d8c96562f1a4b
AgostinaCipolla <133062972+AgostinaCipolla@users.noreply.github.com>
2023-06-27 21:45:18 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,28 @@
+function copy() {alert ("Agostina Cipolla -- a.cipolla1999@gmail.com -- Mar del Plata, Buenos Aires");
+
+}
+
+function oscuro() { document.getElementById("header").style.backgroundColor="#15202B";
+                    document.getElementById("nav").style.backgroundColor="#15202B";
+                    document.getElementById("navletra1").style.color="#DDDDDD";
+                    document.getElementById("navletra2").style.color="#DDDDDD";
+                    document.getElementById("navletra3").style.color="#DDDDDD";
+                    document.getElementById("navletra4").style.color="#DDDDDD";
+                    document.getElementById("texto").style.color="#DDDDDD";
+                    document.getElementById("section").style.backgroundColor="#15202B";
+                    document.getElementById("section2").style.backgroundColor="#15202B";
+                    document.getElementById("titulo").style.color="#CCCCCC";
+                    document.getElementById("textohis").style.color="#CCCCCC";
+                    document.getElementById("section3").style.backgroundColor="#15202B";
+                    document.getElementById("ingredlis").style.color="#CCCCCC";
+                    document.getElementById("pasoslis").style.color="#CCCCCC";
+                    document.getElementById("section4").style.backgroundColor="#15202B";
+                    document.getElementById("videotitu").style.color="#CCCCCC";
+                    document.getElementById("section5").style.backgroundColor="#15202B";
+                    document.getElementById("seguircoc").style.color="#CCCCCC";
+                    document.getElementById("footer").style.backgroundColor="#15202B";
+                    
+
+
+
+ }
